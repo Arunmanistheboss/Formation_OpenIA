722a58908d2eb1703e159f0581fc3f5dd758aa99
Arun <arun2013ane@gmail.com>
2024-12-16 16:53:48 +0100

    My Coomit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 import streamlit as st
 
-client = OpenAI(api_key="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")
+client = OpenAI()
 
 value = st.text_input("Prompt")
 if(value):
