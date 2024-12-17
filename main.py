4c8884b88468c6b264d70024b30c9613e8ba3ad6
Arun <arun2013ane@gmail.com>
2024-12-17 09:17:31 +0100

    Neffft

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from openai import OpenAI
 import streamlit as st
 
-client = OpenAI(api_key="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")
+client = OpenAI()
 
-value = st.text_input("Prompt")
-if(value):
+value = st.text_input("Prompt...")
+if (value):
     txt = st.header("Waiting for api...")
 
     completion = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
-        {"role": "user", "content" : "value"}
-    ] 
-)   
+        {"role": "user","content": value}
+    ]
+    )
 
-# print(completion.choices[0].message.content)
-
-st.write(completion.choices[0].message.content)
+    txt.text(completion.choices[0].message.content)
