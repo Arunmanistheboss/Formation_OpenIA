855a49323b75cefcd1ce0b3ec247eca6f65f00c3
Arun <arun2013ane@gmail.com>
2024-12-17 11:05:57 +0100

    Conversation

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from openai import OpenAI
 import streamlit as st
-
+ 
 client = OpenAI()
 
-value = st.chat_input("Prompt...")
-if (value):
-    with(st.chat_message("user")):
-        st.write(value)
-        
-    with(st.chat_message("assistant")):
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
+def new_message(content: str):
+    with (st.chat_message("user")):
+        st.session_state.messages.append({"role":"user", "content": content})
+        st.write(content)
 
-        txt = st.header("Waiting for api...")
-
+    with (st.chat_message("assistant")):
+        field = st.text("waiting for an answer...")
+    
         completion = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
-            {"role": "user","content": value}
+            {"role": "user","content": content}
         ]
         )
+        
+        st.session_state.messages.append({"role": "assistant", "content": completion.choices[0].message.content})
+        field.text(completion.choices[0].message.content)
 
-        txt.text(completion.choices[0].message.content)
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.text(message["content"])
+value = st.chat_input("Say something")
+if (value and value != ""):
+    new_message(value)
+    value = ""
