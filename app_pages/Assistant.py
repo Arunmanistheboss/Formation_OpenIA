82b301e392a4c79273c6d066ff6657a8cfd8c70d
Arun <arun2013ane@gmail.com>
2024-12-18 11:20:37 +0100

    Assitant Vocal

---
+++ app_pages/Assistant.py
@@ -0,0 +1,48 @@
+from openai import OpenAI  
+import streamlit as st  
+from pathlib import Path  
+
+
+client = OpenAI()
+
+
+audio = st.audio_input("Dites quelque chose")
+
+if (audio):
+    file_path = Path(__file__).parent.parent / "input.mp3"
+
+    with open(file_path, "wb") as file:
+        file.write(audio.getbuffer())  
+
+    txt = st.text("Waiting for API")
+
+    with open(file_path, "rb") as file:
+        audiotranscript = client.audio.transcriptions.create(
+            model="whisper-1",  
+            file=file
+        )
+
+    st.subheader("Texte transcrit :")
+    st.write(audiotranscript.text)  
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",  
+        messages=[{"role": "user", "content": audiotranscript.text}]  
+    )
+
+    output = completion.choices[0].message.content
+
+    st.subheader("Réponse générée par GPT :")
+    st.write(output)  
+
+    response = client.audio.speech.create(
+        model="tts-1",  
+        voice="alloy",  
+        input=output  
+    )
+
+    file_path = Path(__file__).parent.parent / "output.mp3"
+
+    response.stream_to_file(file_path)
+
+    st.audio(file_path, autoplay=True)  
--- main.py
+++ main.py
@@ -7,6 +7,10 @@ pg = st.navigation([
         st.Page(path + "Chat.py", title="1.Chat"), 
         st.Page(path + "DALL-E.py", title="2.DALL-E"),
         st.Page(path + "Article.py", title="3.Article"),
+        st.Page(path + "whisper.py", title="4.Whisper"),
+        st.Page(path + "tts.py", title="5.TTS"),
+        st.Page(path + "Assistant.py", title="5.Assistant Vocal"),
+
 
 
     ]) 
